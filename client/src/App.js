555629f44ec5271a8884a522d228a1f1885e7ef2
Grant Crawford <grantttt.crawford@gmail.com>
2023-05-04 18:05:48 -0500

    filter multiple

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,38 @@ import { Pagination } from '@mui/material';
 function App() { 
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
-  const [genreFilter, setGenreFilter] = useState('');
+  const [genreFilter, setGenreFilter] = useState([]);
+const [genreQueryParam, setGenreQueryParam] = useState('');
+  const [selectedGenres, setSelectedGenres] = useState([]);
   const [page, setPage] = useState(0);
   const [totalElements, setTotalElements] = useState(0);
 
-  const fetchMovies = async (query = '', genre = '', pageNumber = 0) => {
+  const genreOptions = [
+    'Action',
+    'Adventure',
+    'Animation',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'History',
+    'Horror',
+    'Music',
+    'Mystery',
+    'Romance',
+    'Science Fiction',
+    'TV Movie',
+    'Thriller',
+    'War',
+    'Western'
+  ];
+
+  const fetchMovies = async (query = '', genres = [], pageNumber = 0) => {
+    const genreQueryParam = genres.length > 0 ? genres.join(',') : '';
     const response = await fetch(
-      `http://localhost:8080/?title=${encodeURIComponent(query)}&genre=${encodeURIComponent(genre)}&page=${pageNumber}&size=20&sort=title&direction=ASC`
+      `http://localhost:8080/?title=${encodeURIComponent(query)}&genre=${encodeURIComponent(genreQueryParam)}&page=${pageNumber}&size=20&sort=title&direction=ASC`
     );
 
     const data = await response.json();
@@ -23,15 +48,16 @@ function App() {
   };
 
   useEffect(() => {
-    fetchMovies(searchQuery, genreFilter, page);
-  }, [searchQuery, genreFilter, page]);
+    fetchMovies(searchQuery, selectedGenres, page);
+  }, [searchQuery, selectedGenres, page]);
   
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
   };
 
-  const handleFilter = (genre) => {
-    setGenreFilter(genre);
+  const handleFilter = (selectedGenres) => {
+    setSelectedGenres(selectedGenres);
+
   };
 
   const handleChangePage = (event, value) => {
@@ -48,7 +74,7 @@ function App() {
             onChange={handleSearch}
             sx={{ width: '50%', marginRight: 2 }}
           />
-          <FilterSidebar onFilter={handleFilter} />
+          <FilterSidebar onFilter={handleFilter} options={genreOptions} />
         </Box>
         <Grid container spacing={2}>
           {movies.map((movie) => (
@@ -70,9 +96,7 @@ function App() {
       </Box>
     </Container>
   );
-  
+          
+          };
 
-  
-  
-}
-export default App;
+          export default App;
--- client/src/components/FilterSidebar.js
+++ client/src/components/FilterSidebar.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 const FilterSidebar = ({ onFilter }) => {
-  const handleFilter = (e) => {
-    onFilter(e.target.value);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const genreOptions = [
+    'Action',
+    'Adventure',
+    'Animation',
+    'Comedy',
+    'Crime',
+    'Documentary',
+    'Drama',
+    'Family',
+    'Fantasy',
+    'History',
+    'Horror',
+    'Music',
+    'Mystery',
+    'Romance',
+    'Science Fiction',
+    'TV Movie',
+    'Thriller',
+    'War',
+    'Western'
+  ];
+
+  const handleChange = (e) => {
+    const selectedOptions = e.target.value;
+    setSelectedGenres(selectedOptions);
+    onFilter(selectedOptions.includes('All Genres') ? '' : selectedOptions);
   };
 
   return (
     <Box>
       <Typography variant="h6">Filter by Genre</Typography>
-      <select onChange={handleFilter}>
-        <option value="">All Genres</option>
-        <option value="action">Action</option>
-        <option value="comedy">Comedy</option>
-        {/* Add more genres as needed */}
-      </select>
+      <FormControl fullWidth variant="outlined" sx={{ mt: 1 }}>
+        <InputLabel htmlFor="genres-select">Genres</InputLabel>
+        <Select
+          labelId="genres-select-label"
+          id="genres-select"
+          multiple
+          value={selectedGenres}
+          onChange={handleChange}
+          label="Genres"
+        >
+          {genreOptions.map((genre) => (
+            <MenuItem key={genre} value={genre}>
+              {genre}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      {selectedGenres.length > 0 && (
+        <Box sx={{ mt: 1 }}>
+          <Typography variant="body2">
+            Selected Genres: {selectedGenres.join(', ')}
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 };
 
 export default FilterSidebar;
-
-
